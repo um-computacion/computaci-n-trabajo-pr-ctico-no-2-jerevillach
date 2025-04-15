c7ec303f6c08b73ef3128f8c66c223905bece161
jerevillach <j.villach@alumno.um.edu.ar>
2025-04-15 16:19:03 +0000

    hice los test para frases no palindromes

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -6,5 +6,14 @@ class Testapalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("level"))
         self.assertFalse(is_palindrome("como"))
         self.assertFalse(is_palindrome("arbol"))
+    def test_frases_palindromes(self):
+        self.assertTrue(is_palindrome("La ruta natural"))
+        self.assertTrue(is_palindrome("Amo la pacífica paloma"))
+        self.assertTrue(is_palindrome("Anita lava la tina"))
+    def test_frases_no_palindromes(self):
+        self.assertFalse(is_palindrome("El sol brilla"))
+        self.assertFalse(is_palindrome("Python es genial"))
+        self.assertFalse(is_palindrome("Hoy es un buen día"))
 if __name__ == "__main__":
-    unittest.main()      
+    unittest.main() 
+    
