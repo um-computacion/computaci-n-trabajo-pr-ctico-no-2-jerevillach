5678f7f0b07949b28fd5266b1cf7b501bb3a6548
jerevillach <j.villach@alumno.um.edu.ar>
2025-04-15 17:04:47 +0000

    hice la funcion para limpieza de texto

---
+++ src/palindrome.py
@@ -0,0 +1,7 @@
+import string
+def is_palindrome(text):
+    text = text.lower()
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+    cleaned_text = cleaned_text.replace(" ", "")
+    return cleaned_text
+
